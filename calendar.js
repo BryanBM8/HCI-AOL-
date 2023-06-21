91164a732f19d64a0cc9be2133c1ce4e89580fb5
Jeselyn Tania <jeselyntan01@gmail.com>
2023-06-21 07:39:22 +0700

    ok

--- calendar.js
+++ calendar.js
@@ -454,3 +454,11 @@ function convertTime(time) {
   time = timeHour + ":" + timeMin + " " + timeFormat;
   return time;
 }
+
+const modeToggle = document.getElementById('mode-toggle');
+    const body = document.body;
+    
+    modeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+        body.classList.toggle('light-mode');
+    });
